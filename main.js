17a0ee642f74cb4de46562312f9d4aac130f2edd
AL MAHMUD SURUJ <mahmudsuruj14@gmail.com>
2021-09-02 00:19:06 +0800

    second commit

--- main.js
+++ main.js
@@ -1,6 +1,52 @@
 const loadSingleUser = () => {
     fetch('https://randomuser.me/api/')
         .then(res => res.json())
+        .then(data => console.log(data.results[0]))
+}
+loadSingleUser();
+
+const displaySingleUser = user => {
+    console.log(user);
+}
+
+const loadMeals = searchText => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        // .then(data => console.log(data))
+        .then(data => displayMeals(data.meals))
+}
+const displayMeals = meals => {
+    const container = document.getElementById('meals');
+    meals.forEach(meal => {
+        console.log(meal);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <h1> ${meal.strMeal}</h1>
+        <button onclick="loadMealDetail('${meal.strMeal}')">click Me</button>
+        `;
+        container.appendChild(div);
+    })
+}
+
+
+
+loadMeals('fish');
+
+const loadMealDetail = mealname =>{
+    console.log(mealname);
+}
+loadMealDetail();
+
+
+
+
+
+
+/* const loadSingleUser = () => {
+    fetch('https://randomuser.me/api/')
+        .then(res => res.json())
+        // .then(data => console.log(data))
         .then(data => displaySingleUser(data.results[0]))
 }
 loadSingleUser();
@@ -57,4 +103,4 @@ loadMeals('fish');
 
 const loadMealDetail = mealName => {
     console.log(mealName);
-}
+} */
